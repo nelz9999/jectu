ae96cf803d98b24178fab57aa5c153edff4a367c
nelz9999 <nelz9999@bda9dba7-ad3d-0410-9311-afdf0168f34f>
2007-11-13 05:59:47 +0000

    Got effective/ineffective working for both primitives and reference types.

--- trunk/jectu/src/test/java/net/nelz/jectu/JectuTest.java
+++ trunk/jectu/src/test/java/net/nelz/jectu/JectuTest.java
@@ -1,6 +1,10 @@
 package net.nelz.jectu;
 
 import static org.testng.AssertJUnit.*;
+
+import java.util.*;
+
+import net.nelz.jectu.objects.*;
 import net.nelz.jectu.primitive.*;
 
 import org.testng.annotations.*;
@@ -64,4 +68,43 @@ public class JectuTest {
 			assertTrue(ex.getMessage().indexOf("ignore") != -1);
 		}
 	}
+	
+	@Test
+	public void shouldSucceedObject() {
+		final Date date1 = new Date();
+		final Date date2 = (Date) date1.clone();
+		assertTrue(date1 != date2);
+		assertTrue(date1.equals(date2));
+		
+		new Jectu(PrimitivePlusObjectPOJO.class)
+			.setEffectiveByDefault(true)
+			.addEffectiveField("testObject", date1, new StringBuffer(), date2)
+			.execute();
+	}
+	
+	@Test
+	public void shouldSucceedObjectsAndPrimitives() {
+		final Date date1 = new Date();
+		final Date date2 = (Date) date1.clone();
+		final Date date3 = new Date(date1.getTime() - 1000000);
+		assertTrue(date1 != date2);
+		assertTrue(date1.equals(date2));
+		assertFalse(date1.equals(date3));
+
+		new Jectu(ObjectsAndPrimitives.class)
+			.setEffectiveByDefault(true)
+			.addEffectiveField("testDate", date1, date3, date2)
+			.addIneffectiveField("xBoolean")
+			.addIneffectiveField("xByte")
+			.addIneffectiveField("xChar")
+			.addIneffectiveField("xDouble")
+			.addIneffectiveField("xFloat")
+			.addIneffectiveField("xInt")
+			.addIneffectiveField("xLong")
+			.addIneffectiveField("xShort")
+			.addIneffectiveField("xDate", date1, date3)
+			.addIneffectiveField("xString", "bubba", "gump")
+			.execute();
+	}
+
 }
